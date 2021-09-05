6ffb33df9910d6d93e23414c3bd211de8b5d453e
NathasyaSR <65170015+nthswny@users.noreply.github.com>
2021-09-05 16:48:04 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+var navLinks = document.getElementById("navLinks");
+// const navLinks = document.querySelector(".nav-links");
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+function hideMenu(){
+    navLinks.style.right = "-200px";  
+}
+
+// const hamburger = document.querySelector(".hamburger");
+// const navLinks = document.querySelector(".nav-links");
+// const links = document.querySelectorAll(".nav-links li");
+
+// hamburger.addEventListener("click", () => {
+//   navLinks.classList.toggle("open");
+//   links.forEach(link => {
+//     link.classList.toggle("fade");
+//   });
+// });
